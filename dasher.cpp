f55b7a55aa4bae6bb1cc8dfbc595a1a590665bd3
Zachary Dowd <zacharyd2796@zohomail.com>
2022-04-10 16:52:19 -0600

    added vertical movement

--- dasher.cpp
+++ dasher.cpp
@@ -2,13 +2,34 @@
 
 int main()
 {
+    // window dimensions
+    const int windowWidth{500};
+    const int windowHeight{500};
 
-    InitWindow(500, 500, "Dapper Dasher");
+    // initialize window
+    InitWindow(windowWidth, windowHeight, "Dapper Dasher");
+
+    // rectangle dimensions
+    const int width{50};
+    const int height{80};
+
+    int posY{windowHeight - height};
+    int velocity{0};
+
     SetTargetFPS(60);
-    while (WindowShouldClose() == false)
+    while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(WHITE);
+        // logic begins here
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+        posY += velocity;
+        DrawRectangle(windowWidth / 2, posY, width, height, BLUE);
+
         EndDrawing();
     }
+    CloseWindow();
 }
