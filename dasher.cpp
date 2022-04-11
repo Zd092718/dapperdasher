4f2c368e3f70cb01dd2ec9b2a8cba9ff625f4a6c
Zachary Dowd <zacharyd2796@zohomail.com>
2022-04-10 20:12:38 -0600

    scarfy's moving

--- dasher.cpp
+++ dasher.cpp
@@ -12,22 +12,34 @@ int main()
     // accelaration due to gravity (pixels/s)/s
     const int gravity{1'000};
 
+    // scarfy model
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
     scarfyRec.width = scarfy.width / 6;
     scarfyRec.height = scarfy.height;
     scarfyRec.x = 0;
     scarfyRec.y = 0;
+
+    // scarfy position
     Vector2 scarfyPos;
     scarfyPos.x = windowWidth / 2 - scarfyRec.width / 2;
     scarfyPos.y = windowHeight - scarfyRec.height;
+
+    // nebula model
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+    Rectangle nebulaRec;
 
     // pixels per second
     const int jumpVel{-600};
     int velocity{0};
 
-    // is scarfy in air?
+    // animation frame
+    int frame{};
 
+    const float updateTime{1.0 / 12.0};
+    float runningTime{};
+
+    // is scarfy in air?
     bool isInAir{};
 
     SetTargetFPS(60);
@@ -60,10 +72,25 @@ int main()
         }
         // update y postion
         scarfyPos.y += velocity * dt;
+
+        // update running time
+        runningTime = runningTime + dt;
+        if (runningTime >= updateTime)
+        {
+            // update animation frame
+            runningTime = 0.0;
+            scarfyRec.x = frame * scarfyRec.width;
+            frame++;
+            if (frame > 5)
+            {
+                frame = 0;
+            }
+        }
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
 
         EndDrawing();
     }
     UnloadTexture(scarfy);
+    UnloadTexture(nebula);
     CloseWindow();
 }
